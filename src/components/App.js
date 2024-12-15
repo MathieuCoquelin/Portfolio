65c6766697a45663aae88d8a1841589f851c54de
MatthieuCoquelin <mathieu.coquelin@outlook.fr>
2024-12-15 18:48:29 +0100

    Remove useless asset

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
-// import logo from '../assets/logo.svg';
 import '../styles/App.css';
 import Layout from './webLayout/Layout'
-// import WebLayout from './WebLayout';
 
 function App() {
   return (
